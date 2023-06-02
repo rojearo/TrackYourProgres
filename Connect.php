6c7e54039a52bef3dbd4ca61f34d8caf3342f31c
rojearo <rojearom@gmail.com>
2023-06-02 13:26:08 +0200

    connect page toegevoegd

--- Connect.php
+++ Connect.php
@@ -1,1 +1,18 @@
 <?php
+
+$servername = "localhost";
+$dbname     = "trackyourprogres";
+$username   = "root";
+$password   = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;  
+                                dbname=$dbname",
+        $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connectie gelukt <br />";
+}
+catch(PDOException $e)
+{
+    echo "connectie mislukt; " . $e->getMessage();
+}
