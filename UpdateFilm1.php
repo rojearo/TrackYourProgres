df8c234b5ae384fb7c9df6adabd599fb88d497ce
rojearo <rojearom@gmail.com>
2023-06-16 15:18:23 +0200

    heb de update formulier 1 gemaakt en toegevoegd

---
+++ UpdateFilm1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+
+<head>
+
+    <title>update Film Formulier 1</title>
+    <link rel="stylesheet" href="tabellen.css"
+</head>
+
+<body>
+
+<div class="form">
+    <h1>update Film formulier 1</h1>
+
+    <form action="updateFilm2.php" method="post">
+
+        <label for="FilmID">FilmID:</label>
+
+        <input type="text" id="FilmID" name="FilmID">
+
+        <input type="submit">
+
+    </form>
+
+    <a href="index.php"><br/>Terug naar het hoofdmenu</a>
+
+</div>
+</body>
