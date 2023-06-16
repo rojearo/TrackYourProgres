ee2ec5ab5164292e796d4d20cb63ea16bf12c10b
rojearo <rojearom@gmail.com>
2023-06-16 15:47:00 +0200

    heb de update formulier 3 bijgewerkt

--- Updatefilm3.php
+++ Updatefilm3.php
@@ -1,1 +1,30 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>update Film formulier 3</title>
+</head>
+<body>
+<h1>update Film formulier 3</h1>
+
 <?php
+require "films.php";
+
+// gegevens uit de array in variabelen stoppen
+$FilmID = $_POST["FilmID"];
+$FilmNaam=$_POST["FilmNaam"];
+$FilmBeschrijving=$_POST["FilmBeschrijving"];
+$FilmRating=$_POST["FilmRating"];
+$FilmBanner=$_POST["FilmBanner"];
+
+// maken object ---------------------------------------------------
+$Film1 = new film ($FilmID, $FilmNaam, $FilmBeschrijving, $FilmRating, $FilmBanner);
+$Film1->updateFilm($FilmID);
+echo "Dit zijn de gewijzigde gegevens: <br/>";
+echo $FilmID."<br/>";
+$Film1->oplezen();
+
+?>
+<a href="artikelen.html">Terug naar het hoofdmenu</a>
+</body>
+</html>
